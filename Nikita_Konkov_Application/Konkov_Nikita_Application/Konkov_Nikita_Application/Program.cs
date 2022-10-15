53c5e00b1f859059154175b2b8e0f24bb6509f16
Nikita Konkov <maks.gromov97@gmail>
2022-10-15 10:55:04 +0300

    Done home work

--- Nikita_Konkov_Application/Konkov_Nikita_Application/Konkov_Nikita_Application/Program.cs
+++ Nikita_Konkov_Application/Konkov_Nikita_Application/Konkov_Nikita_Application/Program.cs
@@ -1,5 +1,147 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 
-Console.ReadLine();
+// This is my solution of additional task >
+
+// The C# programming language was designed by Anders Hejlsberg from Microsoft in 2000
+// and was later approved as an international standard by Ecma (ECMA-334) in 2002
+// and ISO/IEC (ISO/IEC 23270) in 2003. Microsoft introduced C# along with .NET
+// Framework and Visual Studio, both of which were closed-source.
+
+//string str1 = Console.ReadLine();
+//string str2 = Console.ReadLine();
+//string str3 = Console.ReadLine();
+//string str4 = Console.ReadLine();
+
+
+//StringBuilder str5 = new StringBuilder(str1);
+//str5.Append(str2);
+//str5.Append(str3);
+//str5.Append(str4);
+
+//Console.WriteLine(str5);
+
+//Console.ReadLine();
+
+//                       End Here
+
+new Homework().Task_1();
+
+class Homework
+{
+    public void Task_1()
+    {
+        //int num = 1;
+        //double pSign = 3.14;
+        //bool gravitation = true;
+        //string nickname = "Moment_schastia";
+        //char justSign = 'M';
+
+        //Console.WriteLine(num);
+        //Console.WriteLine(pSign);
+        //Console.WriteLine(gravitation);
+        //Console.WriteLine(nickname);
+        //Console.WriteLine(justSign);
+
+        //Console.ReadLine();
+    }
+    public void Task_2()
+    {
+
+        //int numb1 = 23;
+        //int num2 = 23;
+
+        //var compare1 = numb1 == num2;
+        //var compare2 = numb1.Equals(num2);
+        //Console.WriteLine($"Value for == is {compare1} and value for Equils is {compare2}");
+
+        //string name1 = "Nikita";
+        //string name2 = "Nikita";
+
+        //var compare3 = name1 == name2;
+        //var compare4 = name1.Equals(name2);
+        //Console.WriteLine($"Value for == is {compare2} and value for Equils is {compare4}");
+        //Console.ReadLine();
+    }
+    public void Task_3()
+    {
+        //Console.WriteLine("Enter name, last name, age (use the ',' sign to devide data's parts)");
+        //string line = Console.ReadLine();
+        //string[] arr1 = line.Split(new char[] { ',' });
+
+        //string str1 = "\"Who are you, stranger!\" - asked the wizard.";
+
+        //Console.WriteLine(str1);
+        //Console.WriteLine("\"I'm " + arr1[0] + arr1[1] + ", my age is" + arr1[2] + " \" - I replied");
+
+        //Console.ReadLine();
+    }
+    public void Task_4()
+    {
+        //What shall we do with the drunken sailor ?
+        //string str1 = Console.ReadLine();
+        //string[] arr1 = str1.Split(new char[] { ' ' });
+
+        //string str2 = string.Join(" ", arr1);
+        //Console.WriteLine(str2);
+
+        //var str3 = " ";
+        //for (int i = 0; i < arr1.Length - 1; i++)
+        //{
+        //    str3 = string.Concat(arr1[i], arr1[i + 1]);
+        //    arr1[i + 1] = str3;
+        //}
+
+        //}
+        //Console.WriteLine(str3);
+        //Console.ReadLine();
+    }
+    public void Task_5()
+    {
+        //It is what it is
+
+        //string str1 = Console.ReadLine(); 
+        //string str2 = str1.Substring(2);
+        //string str3 = str1.Substring(1);
+        //string str4 = str1.Substring(2);
+        //string str5 = string.Join(str2, str4);
+
+        //Console.WriteLine(str5);
+
+
+        //Console.ReadLine();
+    }
+    public void Task_6()
+    {
+        //the roses are red the violets are blue
+        //white, black
+        //string str1 = Console.ReadLine();
+        //string str2 = Console.ReadLine();
+
+        //string[] arr1 = str2.Split(new char[] { ',' });
+        //string str3 = arr1[0];
+        //string str4 = arr1[1];
+        //str4 = str4.Substring(1);
+
+        //string str5 = str1.Replace("red", str3);
+        //string str6 = str5.Replace("blue", str4);
+
+        //Console.WriteLine(str6);
+    }
+    public void Task_7()
+    {
+        //the roses are red the violets are blue
+        //    string str1 = Console.ReadLine();
+        //    Console.WriteLine(str1.ToUpper());
+
+        //    Console.ReadLine();
+    }
+
+}
+   
+
+
