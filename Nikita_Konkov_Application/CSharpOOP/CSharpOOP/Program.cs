f52c04f824d30d82f24e28ec748edd32ff174fb4
Nikita Konkov <maks.gromov97@gmail>
2022-10-28 09:42:15 +0300

    add homework Sharp oop

---
+++ Nikita_Konkov_Application/CSharpOOP/CSharpOOP/Program.cs
@@ -0,0 +1,141 @@
+﻿using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+new Homework_4().Task_5();
+
+class Homework_4
+{
+    public void Task_1()
+    {
+        //Console.Write("Enter a quantity of food portions - ");
+        //float foodPortion = float.Parse(Console.ReadLine());
+        //Console.Write("Enter a quantity of drink bottles - ");
+        //float drinkBottle = float.Parse(Console.ReadLine());
+        //Console.Write("Enter a quantity of souces - ");
+        //float souce = float.Parse(Console.ReadLine());
+
+
+        //AnOrder(foodPortion, 98.4f, drinkBottle, 28.7f, souce, 0.8f, 12);
+
+        //void AnOrder(float foodPortion, float foodCost, float drinkBottle, float drinkCost, float souce, float souceCost, float discount)
+        //{
+
+        //    float theSumCost = (foodPortion * foodCost) + (drinkBottle * drinkCost) + (souce * souceCost);
+        //    float theDiscount = theSumCost * discount / 100;
+
+        //    Console.WriteLine($"The total cost - {theSumCost}. The discount - {theDiscount}. The final amount to the payment - {theSumCost - theDiscount}");
+        //    Console.ReadLine();
+
+        //}
+
+    }
+    public void Task_2()
+    {
+        //Console.Write("Enter the wanted food - ");
+        //string foodName = Console.ReadLine();
+        //Console.Write("Enter the quantity of food portions - ");
+        //string foodPortion = Console.ReadLine();
+
+        //Console.Write("Enter the wanted drink - ");
+        //string drinkName = Console.ReadLine();
+        //Console.Write("Enter the quantity of drink bottles - ");
+        //string drinkBottle = Console.ReadLine();
+
+        //Console.Write("Enter the wanted souce - ");
+        //string souceName = Console.ReadLine();
+        //Console.Write("Enter a quantity of souces - ");
+        //string souce = Console.ReadLine();
+
+
+        //AnOrder(foodPortion, "98.4", drinkBottle, "28.7", souce, "0.8", "12", foodName, drinkName, souceName);
+
+        //void AnOrder(string foodPortion, string foodCost, string drinkBottle, string drinkCost, string souce, string souceCost, string discount, string foodName, string drinkName, string souceName)
+        //{
+
+        //    float theSumCost = (float.Parse(foodPortion) * float.Parse(foodCost)) + (float.Parse(drinkBottle) * float.Parse(drinkCost)) + (float.Parse(souce) * float.Parse(souceCost));
+        //    float theDiscount = theSumCost * float.Parse(discount) / 100;
+
+        //    Console.WriteLine($"The cost of one {foodName} - {foodCost}");
+        //    Console.WriteLine($"The cost of one {drinkName} - {drinkCost}");
+        //    Console.WriteLine($"The cost of one {souceName} - {souceCost}");
+        //    Console.WriteLine($"The total cost - {theSumCost}. The discount - {theDiscount}. The final amount to the payment - {theSumCost - theDiscount}");
+        //    Console.ReadLine();
+        //}
+    }
+    public void Task_3()
+    {
+        //Console.Write("Enter the wanted food - ");
+        //string foodName = Console.ReadLine();
+
+        //Console.Write("Enter the wanted drink - ");
+        //string drinkName = Console.ReadLine(); 
+
+        //Console.Write("Enter the wanted souce - ");
+        //string souceName = Console.ReadLine();
+
+
+        //AnOrder("98.4", "28.7", "0.8", "12", foodName, drinkName, souceName);
+
+        //void AnOrder(string foodCost, string drinkCost, string souceCost, string discount, string foodName, string drinkName, string souceName, string foodPortion = "12", string drinkBottle = "14", string souce = "8")
+        //{
+
+        //    float theSumCost = (float.Parse(foodPortion) * float.Parse(foodCost)) + (float.Parse(drinkBottle) * float.Parse(drinkCost)) + (float.Parse(souce) * float.Parse(souceCost));
+        //    float theDiscount = theSumCost * float.Parse(discount) / 100;
+
+        //    Console.WriteLine($"The cost of one {foodName} - {foodCost}");
+        //    Console.WriteLine($"The cost of one {drinkName} - {drinkCost}");
+        //    Console.WriteLine($"The cost of one {souceName} - {souceCost}");
+        //    Console.WriteLine($"The total cost - {theSumCost}. The discount - {theDiscount}. The final amount to the payment - {theSumCost - theDiscount}");
+        //    Console.ReadLine();
+        //}
+    }
+    public void Task_4()
+    {
+        //Console.Write("Enter length of the side: ");
+        //int sideLength = int.Parse(Console.ReadLine());
+
+        //Console.ReadLine();
+
+        //PentagonPerimeterCaunter(sideLength, sideLength, sideLength, sideLength, sideLength);
+        //SquarePerimeterCaunter(sideLength, sideLength, sideLength, sideLength);
+        //TrianglePerimeterCaunter(sideLength, sideLength, sideLength);
+
+        //void PentagonPerimeterCaunter(params int[] sidesInArray)
+        //{
+        //    int result = 0;
+        //    foreach (var n in sidesInArray)
+        //    {
+        //        result += n;
+
+        //    }
+        //    Console.WriteLine($"Perimetr of Pentagon is {result}");
+        //    Console.ReadLine();
+        //}
+
+        //void SquarePerimeterCaunter(params int[] sidesInArray)
+        //{
+        //    int result = 0;
+        //    foreach (var n in sidesInArray)
+        //    {
+        //        result += n;
+
+        //    }
+        //    Console.WriteLine($"Perimetr of Square is {result}");
+        //    Console.ReadLine();
+        //}
+
+        //void TrianglePerimeterCaunter(params int[] sidesInArray)
+        //{
+        //    int result = 0;
+        //    foreach (var n in sidesInArray)
+        //    {
+        //        result += n;
+
+        //    }
+        //    Console.WriteLine($"Perimetr of Triang is {result}");
+        //    Console.ReadLine();
+    }
+
+}
