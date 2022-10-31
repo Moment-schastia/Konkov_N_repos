1785026deaa791c56f603ade78278de09ec150fb
Nikita Konkov <maks.gromov97@gmail>
2022-10-31 17:42:26 +0300

    add homework CSharpOOP2

---
+++ Nikita_Konkov_Application/CSharpOOP2/CSharpOOP2/BookInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOOP2
+{
+    static class BookInfo
+    {
+        
+        public static void InfoBookIt(Book bookIt) // out of fantasy, sorry
+        {
+             Console.WriteLine($"Here we have {bookIt.Tittle} that was written by {bookIt.Author} in {bookIt.Date}. In book {bookIt.Pages}. And the cost of the book just {bookIt.Cost}");
+           
+        }
+        public static double BooItCostWithDiscaunter(Book bookIt, double discount = 0.17)
+        {
+            double discounted = bookIt.Cost - (bookIt.Cost * discount);
+            return discounted;
+        } 
+        internal static void BookUseWarning()
+        {
+            Console.WriteLine("Read only with a good lighting and don't be ashamed of using glasses");
+
+        }
+
+
+    
+    
+    
+    
+    
+    }
+
+}
