9a70fdbfc6bbfa819034915ba1de307524a95194
Nikita Konkov <maks.gromov97@gmail>
2022-12-16 15:42:26 +0300

    corrected some mastakes of homework

--- Nikita_Konkov_Application/Selenium_Basics/Selenium_Basics/EpamSiteBrowsingTest.cs
+++ Nikita_Konkov_Application/Selenium_Basics/Selenium_Basics/EpamSiteBrowsingTest.cs
@@ -1,31 +1,36 @@
-﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
 
-namespace Selenium_Basics
+namespace SeleniumTest
 {
     [TestFixture]
-    public class EpamHowWeDoItOurWorkNavigationTest
+    public class EpamSiteBrowsingTest
     {
         private IWebDriver webDriver = new ChromeDriver();
+        private string _epamMainLink;
         private string _epamHowWeDoItLink;
         private string _epamOurWorkLink;
 
         [SetUp]
         public void Setup()
         {
+            _epamMainLink = "https://www.epam.com/";
             _epamHowWeDoItLink = "https://www.epam.com/how-we-do-it";
             _epamOurWorkLink = "https://www.epam.com/our-work";
         }
 
         [Test]
-        public void CheckHowWeDoItAndOurWorkNavigation()
+        public void CheckNavigationToEpamSite()
         {
             webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(_epamMainLink);
+
+            Assert.AreEqual(_epamMainLink, webDriver.Url, "You are not in main epam page!");
+        }
+
+        [Test]
+        public void CheckHowWeDoItAndOurWorkNavigation()
+        {
             webDriver.Navigate().GoToUrl(_epamHowWeDoItLink);
             webDriver.Navigate().GoToUrl(_epamOurWorkLink);
             webDriver.Navigate().Refresh();
