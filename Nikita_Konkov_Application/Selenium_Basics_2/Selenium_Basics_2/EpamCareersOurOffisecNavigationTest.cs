171c007d6a0010c704001a7218ec565246071185
Nikita Konkov <maks.gromov97@gmail>
2023-01-03 16:18:02 +0300

    Tried to correct and did all my best

--- Nikita_Konkov_Application/Selenium_Basics_2/Selenium_Basics_2/EpamCareersOurOffisecNavigationTest.cs
+++ Nikita_Konkov_Application/Selenium_Basics_2/Selenium_Basics_2/EpamCareersOurOffisecNavigationTest.cs
@@ -1,34 +1,44 @@
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
+using NUnit.Framework.Constraints;
 
 namespace Selenium_Basics_2
 {
     public class EpamCareersOurOffisecNavigationTest
     {
-        private IWebDriver webDriver = new ChromeDriver();
-        private string _epamMainLink;
+        private IWebDriver _webDriver { get; set; }
+        private string _epamMainLink = "https://www.epam.com/";
+        private string _careerLInk = "https://www.epam.com/careers";
+        private List<string> _expectedOurOfficesList;
 
         [SetUp]
         public void Setup()
         {
-            _epamMainLink = "https://www.epam.com/";
+            _webDriver = new ChromeDriver();
+            _expectedOurOfficesList = new List<string> { "AMERICAS", "EMEA", "APAC" };
         }
 
         [Test]
-        public void CheckNavigationToEpamSite()
+        public void CheckOurOfficesCareerPageSite()
         {
-            webDriver.Manage().Window.Maximize();
-            webDriver.Navigate().GoToUrl(_epamMainLink);
-            webDriver.FindElement(By.XPath("//*[@href =\"/careers\"]")).Click();
-            var ourOffices = webDriver.FindElement(By.XPath("//*[@class = 'tabs__ul js-tabs-links-list']"));
-            var ourOfficesElementList = ourOffices.FindElements(By.XPath(".//div")); // the second part of task, i have no idea how to do
-            Assert.AreEqual("https://www.epam.com/careers", webDriver.Url, "You are not in main epam page!");
-        }
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl(_epamMainLink);
+            _webDriver.FindElement(By.XPath("//*[@href =\"/careers\"]")).Click();
+            var ourOffices = _webDriver.FindElement(By.XPath("//*[@class = 'tabs__ul js-tabs-links-list']"));
+            var ourOfficesElementList = ourOffices.FindElements(By.XPath(".//div")).ToList();
+
+            Assert.AreEqual(_careerLInk, _webDriver.Url, "You are not in main epam page!");
+
+            var realOfficeListText = ourOfficesElementList.Where(l => l.Text.Equals(_expectedOurOfficesList.Any()));
+
+            Assert.AreEqual(_expectedOurOfficesList, realOfficeListText, "Unexpected Offises' list");
+            
+        }   
 
         [TearDown]
         public void End()
         {
-            webDriver.Quit();
+            _webDriver.Quit();
         }
     }
 }
---
+++ Nikita_Konkov_Application/Selenium_Basics_2/Selenium_Basics_2/EpamSearchTests.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Selenium_Basics_2
+{
+    public class EpamSearchTests
+    {
+        private IWebDriver _webDriver { get; set; }
+        private string _epamMainLink = "https://www.epam.com/";
+        private string _epamSearchResultAutomation = "https://www.epam.com/search?q=Automation";
+        private string _epamSearchResultBusinAnalys = "https://www.epam.com/search?q=Business+Analysis";
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDriver = new ChromeDriver();
+        }
+
+
+        [TestCase("Automation")]
+        public void CheckAutomationSearchResult(string wordToSearch)
+        {
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl(_epamMainLink);
+            _webDriver.FindElement(By.XPath("//*[@class = \"header-search__button header__icon\"]")).Click();
+            _webDriver.FindElement(By.XPath("//*[@id = \"new_form_search\"]")).SendKeys(wordToSearch);
+            _webDriver.FindElement(By.XPath("//*[@class = \"header-search__submit\"]")).Click();
+            Assert.AreEqual(_epamSearchResultAutomation, _webDriver.Url, "You are not in Automation search page!");
+
+            var automatinResultList = _webDriver.FindElements(By.XPath("//*[@class=\"search-results__item\"]")).Take(5);
+            var sortedResultList = automatinResultList.Where(l => l.Text.Contains(wordToSearch));
+            Assert.AreEqual(automatinResultList, sortedResultList, "The search results are not correct");
+        }
+
+        [TestCase("Business Analysis")]
+        public void CheckBusinesAnalisResult(string wordToSearch)
+        {
+            _webDriver.Manage().Window.Maximize();
+            _webDriver.Navigate().GoToUrl(_epamMainLink);
+            _webDriver.FindElement(By.XPath("//*[@class = \"header-search__button header__icon\"]")).Click();
+            _webDriver.FindElement(By.XPath("//*[@id = \"new_form_search\"]")).SendKeys(wordToSearch);
+            _webDriver.FindElement(By.XPath("//*[@class = \"header-search__submit\"]")).Click();
+            Assert.AreEqual(_epamSearchResultBusinAnalys, _webDriver.Url, "You are not in Automation search page!");
+
+            var firstSearchResult = _webDriver.FindElement(By.XPath("//*[@class = 'search-results__item']"));// i don't why, but appears not clickable element exception. I've tried defferent xpathes  
+            firstSearchResult.Click();
+            var firstResulrPage= _webDriver.FindElement(By.XPath("//*[contains(@class, 'title-ui title--center')]"));
+            Assert.AreEqual(firstSearchResult.Text, firstResulrPage.Text, "Title of first result is not equal to its page title");
+        }
+
+        [TearDown]
+        public void End()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
