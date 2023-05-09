65c4afa51067bb3f3f2de7d414984e63a496fdda
Nikita Konkov <maks.gromov97@gmail>
2023-05-09 11:26:19 +0300

    modified decorator

--- Nikita_Konkov_Application/CSharpFactoryMethod/CSharpDecorator/Program.cs
+++ Nikita_Konkov_Application/CSharpFactoryMethod/CSharpDecorator/Program.cs
@@ -10,3 +10,22 @@ gigantosaurus = new ExtraLoudGigantosaurusDecorator(gigantosaurus);
 Console.WriteLine(gigantosaurus.Roar());
 
 Console.ReadLine();
+
+
+abstract class Quest
+{
+    protected string Name;
+
+    public string GetName()
+    {
+        return Name;
+    }
+}
+
+class Acatmane : Quest
+{
+    public Acatmane()
+    {
+        Name = "WTF";
+    }
+}
